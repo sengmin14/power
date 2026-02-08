afceb380eccf93a8f94e09f14b20c73400a86f96
Jin SeongMin <sengmin14@naver.com>
2026-02-08 17:19:57 +0900

    cicd적용 확인

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>New React222333</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is123 {count}
         </button>
         <p>
           rhinod, skim
