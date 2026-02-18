502d6ac24a7b7633ee8bacc974ec8bb97a881931
rhinod <rhinod@naver.com>
2026-02-19 01:22:57 +0900

    npm install 테스트

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
 
     <Routes>
-      <Route path='/' element={ <Layout/> }>
+      <Route path='/' element={ <Layout/>  }>
         <Route index element={ <LoginPage/> } />
         <Route path='/join' element={ <JoinPage/> } />
       </Route>
