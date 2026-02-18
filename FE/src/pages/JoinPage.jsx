621e61c0902455153ed8b5e7cbe4a73224ccb2eb
rhinod <rhinod@naver.com>
2026-02-19 03:23:28 +0900

    회원가입 기능완료 - 공통화 작업 필요

--- FE/src/pages/JoinPage.jsx
+++ FE/src/pages/JoinPage.jsx
@@ -19,10 +19,34 @@ const JoinPage = () => {
         });
     }
 
-    const handleSignUp = () => {
+    const handleSignUp = async () => {
         console.log(signData);
 
-        
+        // TODO
+        // 1. 호출 방식 선택 fetch or axios 회의필요 - 서버 주소 공통화
+        // 2. 공통 함수로 api 처리하기 
+        // 3. pw 체크 로직
+        // 4. input validation 로직
+        // 5. 로그인 성공 후 처리
+
+        try {
+            const response = await fetch("http://15.164.100.162:8080/users/auth/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body : JSON.stringify(signData)
+            });
+
+            if( response.ok ) {
+                const result = await response.json();
+                alert("회원가입 성공!");
+                console.log("### result :: ", result);
+            }
+        }
+        catch(err) {
+            console.log("### err :: ", err);
+        }
     }
 
     return(
