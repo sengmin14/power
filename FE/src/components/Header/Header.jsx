fc75aefb1024c833c14ec1e4999d913a55a504bb
rhinod <rhinod@naver.com>
2026-02-23 02:27:07 +0900

    헤더 클릭 시 메인 페이지로 이동

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate("/");
+    }
+
     return(
-        <div className={styles.header}>
+        <div className={styles.header} onClick={handleClick}>
             Tomato's Catch Mind
         </div>
     );
